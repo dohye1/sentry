06073ee976f3e6114376a39fec1e6a6df8ff0d1d
dohye1 <tsc03018@gmail.com>
2023-02-05 13:53:43 +0900

    :package: @sentry/brower to @sentry/react

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserTracing } from "@sentry/tracing";
-import * as Sentry from "@sentry/browser";
+import * as Sentry from "@sentry/react";
 
 Sentry.init({
   dsn: "https://2ba54f691cec47eb90bba09fc768acea@o4504581152768000.ingest.sentry.io/4504586599727104",
